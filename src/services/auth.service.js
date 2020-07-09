976dcfc53038cdbaece18ab0a6fa02863f6dfa23
Attilio Furci <attilio.furci@klaroweb.ch>
2020-07-09 16:26:19 +0200

    dev 0.1

---
+++ src/services/auth.service.js
@@ -0,0 +1,32 @@
+import axios from 'axios'
+
+const API_URL = 'http://localhost:3000/api/'
+
+class AuthService {
+	login(user){
+		return axios
+			.post(API_URL + 'login', {
+				email: user.username,
+				password: user.password
+			})
+			.then(response => {
+				if(response.data.accessToken){
+					localStorage.setItem('user', JSON.stringify(response.data))
+				}
+				return response.data
+			})
+	}
+	logout(){
+		localStorage.removeItem('user')
+	}
+	register(user){
+		return axios
+			.post(API_URL + 'register', {
+				username: user.name,
+				email: user.email,
+				password: user.password
+			})
+	}
+}
+
+export default new AuthService()
